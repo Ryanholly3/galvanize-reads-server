6e62d8a9502f4355cb6abf8d0d233ca584244271
ryanholly3 <ryanholly3@gmail.com>
2018-12-04 12:15:04 -0700

    adjusted migration files

--- migrations/20181204112735_book.js
+++ migrations/20181204112735_book.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('book', function(table) {
         table.increments();
-        table.string('title', 20);
-        table.string('genre', 20);
+        table.string('title', 50);
+        table.string('genre', 50);
         table.text('description');
         table.text('cover_url');
     });
