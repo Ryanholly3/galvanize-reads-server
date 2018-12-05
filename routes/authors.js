791b6e43d55a4a507ad1eedaab2bcd0d22e6a11c
ryanholly3 <ryanholly3@gmail.com>
2018-12-05 10:07:24 -0700

    added author post and delete routes

--- routes/authors.js
+++ routes/authors.js
@@ -9,4 +9,27 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res, next) => {
+    const body = req.body;
+    knex('author')
+      .insert(body)
+      .returning('*')
+      .then(author => {
+        res.json({ author: author[0] });
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+		const id = req.params.id
+    knex('author')
+    	.where('id', id)
+			.del()
+    	.returning('*')
+    	.then(author => {
+        if(!author.length){
+          next()
+        } res.json({ author: author[0] });
+    });
+});
+
 module.exports = router
