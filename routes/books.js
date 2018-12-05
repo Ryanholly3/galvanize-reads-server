4239d9862050083aa0f346d74a9326bce1da7e9e
ryanholly3 <ryanholly3@gmail.com>
2018-12-05 11:46:57 -0700

    added books join

--- routes/books.js
+++ routes/books.js
@@ -2,12 +2,43 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../db/connection')
 
+function getBooks(){
+  return knex('book')
+    .select(
+      'book.id as book_id',
+       'book.title',
+       'book.description',
+       'book.cover_url',
+     )
+}
+
+function authorBookJoin(book){
+  return knex('author_book')
+    .select(
+      'author.id as author_id',
+      'author.first_name',
+      'author.last_name'
+    )
+    .innerJoin('author', 'author.id', 'author_book.author_id')
+    .whereIn('author_book.book_id', [book.book_id])
+}
+
+
 router.get('/', (req, res) => {
-  knex('book')
-    .then(books =>{
-      res.json({ books })
-    })
+
+  function getAuthorsForBook(){
+		return getBooks()
+			.then(function(books){
+				return Promise.all(books.map(async (book)=> {
+						book.authors = await authorBookJoin(book)
+						return book
+					})
+				)
+			}).then(books => res.json({ books }))
+	}
+	getAuthorsForBook()
 })
+
 
 router.post('/', (req, res, next) => {
     const body = req.body;
